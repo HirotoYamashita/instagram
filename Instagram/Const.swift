b63a29da574cd7e4fe7964b3ee73b7810097b700
Hello.Santa <santa@santapc.local>
2018-12-18 16:51:20 +0900

    新規プロジェクトアップロード

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Santa on 2018/12/07.
+//  Copyright © 2018 HirotoYamashita. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by Santa on 2018/12/07.
+//  Copyright © 2018 HirotoYamashita. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    @IBOutlet weak var commentField: UITextField!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    @IBOutlet weak var sendCommentButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setPostData(_ postData: PostData) {
+        
+        self.postImageView.image = postData.image
+        
+        self.captionLabel.text = "\(postData.name!) : \(postData.caption!)"
+        let likeNumber = postData.likes.count
+        likeLabel.text = "\(likeNumber)"
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd"
+        let dateString = formatter.string(from: postData.date!)
+        self.dateLabel.text = dateString
+        
+        if postData.isLiked {
+            let buttonImage = UIImage(named: "like_exist")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        } else {
+            let buttonImage = UIImage(named: "like_none")
+            self.likeButton.setImage(buttonImage, for: .normal)
+        }
+        
+        if let commentName = postData.commenterName , let comment = postData.comment{
+            commentLabel.text = "\(commentName) : \(comment)"
+        } else {
+            commentField.text = ""
+        }
+    }
+}
